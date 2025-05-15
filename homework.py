c3b663d5ef7b7dfdf88c7834d308d4f7ddca041d
HuongBTL <lanhuongbui5999@gmail.com>
2025-05-15 18:06:56 +0700

    Bài tập kiểu dữ liệu string va int

--- homework.py
+++ homework.py
@@ -1,1 +1,40 @@
-print("lan xinh")
+
+#int
+a = 10
+b = 5
+## chia (trả về float))
+'''print(a/b)
+##nhân
+print(a*b)
+##Trừ
+print(a-b)
+## cộng
+print(a+b)
+## chia lấy phần nguyên
+print(5//3)
+## chia lấy phần dư (%)
+print(10%3)
+## Luỹ thừa
+print(2**3)
+#string
+str1 = "Hello"
+str2 = "Minasan"
+## Nối chuỗi
+print(str1+" "+str2)
+## Lặp chuỗi
+print(str1*4)
+## Cắt chuỗi
+print(str1[0:3])
+## Viết hoa toàn bộ
+print("Hello".upper())
+## Viết thường toàn bộ
+print("HEllo".lower())
+## Viết hoa chữ cái đầu
+print("hello minasan".capitalize())
+## Viết hoa từng chữ cái đầu
+print("hello minasan".title())
+## Xoá khoảng trắng đầu cuối
+print(" hello  minasan ".strip())
+'''
+## Thay thế chuỗi con
+print("hello minasan".replace("minasan", "everyone"))
