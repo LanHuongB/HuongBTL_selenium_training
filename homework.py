bd2a12d35c1b0e5525e27fe7513a9b9427e091f4
huongbtl <lanhuongbui5999@gmail.com>
2025-05-19 21:04:50 +0700

    dict type 14/5

--- homework.py
+++ homework.py
@@ -1,1 +1,14 @@
-print("lan xinh")
+# dict
+employee = {
+    "name" : "HuongBTL" ,
+    "age" : 26,
+    "position" : "QA"
+}
+## Lấy tất cả giá trị
+print(employee.values())
+
+## Lấy tất cả key
+print(employee.keys())
+
+## Lấy giá trị cụ thể (nếu không có sẽ trả về null)
+print(employee.get("name"))
