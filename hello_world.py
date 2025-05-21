35798d214a90fa1e8945c9fa5cd81f88fe11b317
HuongBTL <lanhuongbui5999@gmail.com>
2025-05-21 08:28:13 +0700

    btvn axtra 2

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,10 @@
-lan = 'xinh dep'
-print('hello world')
-print(lan)
+lan = 'Hello, Selenium!'
+#print(lan)
+employee = {
+"name": "HuongBTL",
+"age": 26,
+"position": "QA"
+}
+print(employee["name"])
+print("3"+"4")
+type("3")
